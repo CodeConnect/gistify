a3ee1eb3df4c605cda277cf0876d8d326faf534e
Amadeus Wieczorek <git@amadeusw.com>
2015-08-06 19:13:19 -0700

    adds classes for code analysis

---
+++ CodeConnect.Gistify.Extension/CodeAnalysis/CodeAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeConnect.Gistify.Extension.CodeAnalysis
+{
+    class CodeAnalyzer
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns>List whose key is the position and value is the name of a declared variable</returns>
+        public SortedList<int, string> FindDeclarations( /* syntax tree and semantic model? */)
+        {
+            // use a walker to visit all declarations
+            // use a semantic model to discover what actually are these declarations (what do they depend on)
+            // TODO: get compilation of a document
+        }
+    }
+}
---
+++ CodeConnect.Gistify.Extension/CodeAnalysis/VSIntegration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeConnect.Gistify.Extension.CodeAnalysis
+{
+    class VSIntegration
+    {
+
+        internal string GetDocumentSyntaxRoot(string filePath)
+        {
+            var currentSolution = SolutionManager.CurrentSolution;
+            var project = currentSolution.Projects.Where(n => n.Documents.Any(m => m.FilePath == filePath)).FirstOrDefault();
+            var document = project.Documents.Where(n => n.FilePath == filePath).Single();
+            var root = document.GetSyntaxRootAsync();
+            return root.Result;
+        }
+
+    }
+}
