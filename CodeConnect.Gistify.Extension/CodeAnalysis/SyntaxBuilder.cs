4eadc67909b98efa3f2a5eff215e5bed02401414
Amadeus Wieczorek <git@amadeusw.com>
2015-08-17 14:41:34 -0700

    skeleton code for syntax builder

---
+++ CodeConnect.Gistify.Extension/CodeAnalysis/SyntaxBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+
+namespace CodeConnect.Gistify.Extension.CodeAnalysis
+{
+    internal class SyntaxBuilder
+    {
+        internal static SyntaxNode GetSnippet(SyntaxTree snippet, int startPos, int endPos)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static SyntaxNode GetGist(SyntaxNode usingStatements, SyntaxNode snippetCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static SyntaxNode GetUsingStatements(IEnumerable<ObjectInformation> objectInfos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
